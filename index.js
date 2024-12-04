08e80883ce9540f6af6a920729ba1a7b58e14e57
Elijah <iamngunjiri@gmail.com>
2024-12-04 10:43:56 +0300

    Complete arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat = (name) => cats.push(name)
+
+const destructivelyPrependCat = (name) => cats.unshift(name)
+
+const destructivelyRemoveLastCat = () => cats.pop()
+
+const destructivelyRemoveFirstCat = () => cats.shift()
+
+const appendCat = (name) => {
+    let newArray = [...cats]
+    newArray.push(name)
+    return newArray
+}
+
+const prependCat = (name) => {
+    let newArray = [...cats]
+    newArray.unshift(name)
+    return newArray
+}
+
+const removeLastCat = () => {
+    let newArray = [...cats]
+    newArray.pop()
+    return newArray
+}
+
+const removeFirstCat = () => {
+    let newArray = [...cats]
+    newArray.shift()
+    return newArray
+}
